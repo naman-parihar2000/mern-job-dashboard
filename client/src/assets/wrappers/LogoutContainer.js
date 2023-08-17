b7eff75e5b1f46fc2b618d8ea97880c603955807
NamanSParihar <pariharnaman41@gmail.com>
2023-08-17 12:36:20 +0530

    added LogoutContainer component

--- client/src/assets/wrappers/LogoutContainer.js
+++ client/src/assets/wrappers/LogoutContainer.js
@@ -7,6 +7,7 @@ const Wrapper = styled.div`
     align-items: center;
     justify-content: center;
     gap: 0 0.5rem;
+    font-family: inherit;
   }
   .img {
     width: 25px;
@@ -38,6 +39,7 @@ const Wrapper = styled.div`
     cursor: pointer;
     width: 100%;
     height: 100%;
+    font-family: inherit;
   }
 `;
 
