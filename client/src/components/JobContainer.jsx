71413a9a782c0239ae51031883732c40cc6cd9fb
naman singh parihar <pariharnaman41@gmail.com>
2023-09-11 00:31:31 +0530

    all jobs page done

--- client/src/components/JobContainer.jsx
+++ client/src/components/JobContainer.jsx
@@ -1,12 +1,12 @@
 import Job from "./Job";
 import Wrapper from "../assets/wrappers/JobsContainer";
-
+import PageBtnContainer from "./PageBtnContainer";
 import { useAllJobsContext } from "../pages/AllJobs";
 
-const JobContainer = () => {
+const JobsContainer = () => {
   const { data } = useAllJobsContext();
-  const { jobs } = data;
-  if (0) {
+  const { jobs, totalJobs, numOfPages } = data;
+  if (jobs.length === 0) {
     return (
       <Wrapper>
         <h2>No jobs to display...</h2>
@@ -16,12 +16,17 @@ const JobContainer = () => {
 
   return (
     <Wrapper>
+      <h5>
+        {totalJobs} job{jobs.length > 1 && "s"} found
+      </h5>
       <div className="jobs">
         {jobs.map((job) => {
           return <Job key={job._id} {...job} />;
         })}
       </div>
+      {numOfPages > 1 && <PageBtnContainer />}
     </Wrapper>
   );
 };
-export default JobContainer;
+
+export default JobsContainer;
